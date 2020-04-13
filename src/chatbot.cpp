714b67234ad68bd96126789f32d04803fd008ac0
azarrias <adolfo.zarrias@gmail.com>
2020-04-13 10:40:20 +0200

    Make changes to the class ChatBot such that it complies with the Rule of Five

--- src/chatbot.cpp
+++ src/chatbot.cpp
@@ -45,6 +45,86 @@ ChatBot::~ChatBot()
 //// STUDENT CODE
 ////
 
+// copy constructor using deep copy policy
+ChatBot::ChatBot(const ChatBot& source)
+{
+    std::cout << "ChatBot Copy Constructor" << std::endl;
+
+    // data handles (not owned)
+    _currentNode = source._currentNode;
+    _rootNode = source._rootNode;
+    _chatLogic = source._chatLogic;
+
+    // data handles (owned)
+    _image = new wxBitmap(*source._image);
+}
+
+// copy assignment operator
+ChatBot& ChatBot::operator=(const ChatBot& source)
+{
+    std::cout << "ChatBot Copy Assignment Operator" << std::endl;
+
+    if (this == &source)    // protection against self-assignment
+        return *this;
+    delete _image;          // deallocate, the object has lived before
+    
+    // data handles (not owned)
+    _currentNode = source._currentNode;
+    _rootNode = source._rootNode;
+    _chatLogic = source._chatLogic;
+
+    // data handles (owned)
+    _image = new wxBitmap(*source._image);
+
+    return *this;
+}
+
+// move constructor
+ChatBot::ChatBot(ChatBot&& source)
+{
+    std::cout << "ChatBot Move Constructor" << std::endl;
+
+    // data handles (not owned)
+    _currentNode = source._currentNode;
+    _rootNode = source._rootNode;
+    _chatLogic = source._chatLogic;
+
+    // data handles (owned)
+    _image = source._image;
+
+    // invalidate source
+    source._currentNode = nullptr;
+    source._rootNode = nullptr;
+    source._chatLogic = nullptr;
+    source._image = nullptr;
+}
+
+// move assignment operator
+ChatBot& ChatBot::operator=(ChatBot&& source)
+{
+    std::cout << "ChatBot Move Assignment Operator" << std::endl;
+
+    if (this == &source)    // protection against self-assignment
+        return *this;
+    delete _image;          // deallocate, the object has lived before
+
+    // data handles (not owned)
+    _currentNode = source._currentNode;
+    _rootNode = source._rootNode;
+    _chatLogic = source._chatLogic;
+
+    // data handles (owned)
+    _image = source._image;
+
+    // invalidate source
+    source._currentNode = nullptr;
+    source._rootNode = nullptr;
+    source._chatLogic = nullptr;
+    source._image = nullptr;
+
+    return *this;
+}
+
 ////
 //// EOF STUDENT CODE
 
